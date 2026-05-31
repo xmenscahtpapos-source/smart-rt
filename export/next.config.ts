80bebb9d1215769e437a6f86e404bfc67f4fc483
xmenscahtpapos <xmenscahtpapos@gmail.com>
2026-05-31 12:06:34 +0700

    Fix duplicate next config

--- export/next.config.ts
+++ export/next.config.ts
@@ -35,38 +35,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
