25af0784baa38d267471281384abdae043d6da65
xmenscahtpapos <xmenscahtpapos@gmail.com>
2026-05-31 11:46:50 +0700

    Fix Next export for Capacitor

--- export/next.config.ts
+++ export/next.config.ts
@@ -1,4 +1,40 @@
+import type { NextConfig } from 'next';
 
+const nextConfig: NextConfig = {
+  output: 'export',
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
